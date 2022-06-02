71d35096e49074483b4f831e9b6479da4021b7e3
Diego <A00827847@itesm.mx>
2022-06-02 12:34:45 -0500

    created methods

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,12 +1,39 @@
 const UserService = require("../service/users.service");
 
 module.exports = {
+  getUsers: async (req, res, next) => {
+    try {
+      const users = await UserService.getUsers();
+      res.json({users});
+    } catch (err) {
+      res.json({ message: `Error al obtener todos los usuarios. Err: ${err}` });
+    }
+  },
+
   getUserById: async (req, res, next) => {
     try {
       const user = await UserService.getUserById(req.params.id);
       res.json({user});
     } catch (err) {
       res.json({ message: `Error al obtener usuario. Err: ${err}` });
+    }
+  },
+
+  createUser: async (req, res, next) => {
+    try {
+      const user = await UserService.createUser(req.body.email, req.body.password);
+      res.json({user});
+    } catch (err) {
+      res.json({ message: `Error al crear usuario. Err: ${err}` });
+    }
+  },
+
+  deleteUserById: async (req, res, next) => {
+    try {
+      await UserService.deleteUserById(req.params.id);
+      res.json({message: 'Eliminado exitosamente'});
+    } catch (err) {
+      res.json({ message: `Error al eliminar usuario. Err: ${err}` });
     }
   },
 
--- service/users.service.js
+++ service/users.service.js
@@ -3,6 +3,15 @@ const timestamp = new Date();
 const Model = require('../model/user.model');
 
 module.exports = { 
+  getUsers : async () => {
+    try {
+      const users = await Model.User.findAll()
+      return users;
+    } catch (error) { 
+      console.log(`Service error: error al obtener usuarios: Error: ${error}`)
+    }
+  }, 
+
   getUserById : async (id) => {
     try {
       const user = await Model.User.findOne({
@@ -10,7 +19,31 @@ module.exports = {
       })
       return user;
     } catch (error) { 
-      console.log(`Error al obtener usuario: Error: ${error}`)
+      console.log(`Service error: error al obtener usuario: Error: ${error}`)
+    }
+  }, 
+
+  createUser : async (email, password) => {
+    try {
+      const user = await Model.User.create({
+        email: email,
+        hashed_password: password
+      })
+      return user;
+    } catch (error) { 
+      console.log(`Service error: error al crear usuario: Error: ${error}`)
+    }
+  }, 
+
+  deleteUserById : async (id) => {
+    try {
+      await Model.User.destroy({
+        where:{
+          id: id
+        }
+      })
+    } catch (error) { 
+      console.log(`Service error: error al eliminar usuario: Error: ${error}`)
     }
   }, 
 }
