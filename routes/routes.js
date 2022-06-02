d8a8cc3d5cfeccaee4c5714d8539c4dbb9155424
Diego <A00827847@itesm.mx>
2022-06-02 12:45:59 -0500

    added status ok

--- routes/routes.js
+++ routes/routes.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/users', require('./users.route'));
+router.use('/status', require('./status.route'));
 
 module.exports = router;
